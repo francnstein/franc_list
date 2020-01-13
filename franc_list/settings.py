ba9cdf7e7075dd9ed5f256fb9445e5c1d390ae88
francnstein <franczale@icloud.com>
2020-01-12 23:44:09 -0500

    work

--- franc_list/settings.py
+++ franc_list/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
+from django.template.defaulttags import csrf_token
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
--- franc_list/wsgi.py
+++ franc_list/wsgi.py
@@ -12,7 +12,4 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'franc_list.settings')
-
 application = get_wsgi_application()
-from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'franc_list.settings')
